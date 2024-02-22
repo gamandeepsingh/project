817778a19ec5c380ae331f0789c55d0afa1ac145
gamandeepsingh <gamandeepsingh4@gmail.com>
2024-02-22 20:47:13 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Main from "./components/Main";
 import Signup from "./components/Singup";
 import Login from "./components/Login";
 import EmailVerify from "./components/EmailVerify";
+import Profile from "./components/profile/Profile";
 
 function App() {
 	const user = localStorage.getItem("token");
@@ -12,6 +13,7 @@ function App() {
 			{user && <Route path="/" exact element={<Main />} />}
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
+			<Route path="/profile/update" element={<Profile />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
 			<Route path="/users/:id/verify/:token" element={<EmailVerify />} />
 		</Routes>
